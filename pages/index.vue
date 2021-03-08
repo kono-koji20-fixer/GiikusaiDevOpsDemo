21b860cbfc58d2d021cc1935c0c9e26e7ef4de0d
kono-koji20-fixer <kono.koji20@fixer.co.jp>
2021-03-08 16:58:36 +0900

    :zap: Install Nuxt App

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>開発サイクルを爆速にする！</h1>
+    <h2>~ Azure DevOpsでアプリのビルド・デプロイを自動化 ~</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+}
+</script>
