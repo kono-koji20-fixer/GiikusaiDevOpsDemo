12ecf4bd302d7fdbe5acf54c53b950b245a35760
kono-koji20-fixer <kono.koji20@fixer.co.jp>
2021-03-09 14:04:11 +0900

    :construction_worker: Test pipeline

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,8 @@
   <div>
     <h1>開発サイクルを爆速にする！</h1>
     <h2>~ Azure DevOpsでアプリのビルド・デプロイを自動化 ~</h2>
-    <p>株式会社FIXER Cloud Solution Engineer</p>
+    <p>株式会社FIXER</p>
+    <p>Cloud Solution Engineer</p>
     <p>河野 光司 Koji Kono</p>
   </div>
 </template>
