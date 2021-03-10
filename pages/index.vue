5584a6bbdf619a30cbc1efb5e42afa54c4aa646a
kono-koji20-fixer <kono.koji20@fixer.co.jp>
2021-03-10 18:54:46 +0900

    :sparkles:Update Top Page

--- pages/index.vue
+++ pages/index.vue
@@ -9,6 +9,14 @@
       <v-card-text>河野光司 Koji Kono</v-card-text>
       <v-card-text>Cloud Solution Engineer</v-card-text>
     </v-card>
+    <v-card>
+      <v-card-title>
+        k8sコトハジメ！AzureでサクッとKubernetesを始めてみよう
+      </v-card-title>
+      <v-card-text>株式会社FIXER</v-card-text>
+      <v-card-text>冨本優介 Yusuke Tomimoto</v-card-text>
+      <v-card-text>Cloud Solution Engineer</v-card-text>
+    </v-card>
   </div>
 </template>
 
